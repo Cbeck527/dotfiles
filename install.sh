791a0efcb71e904cb1f4d21a389af20571efd000
Chris Becker <cbeck527@gmail.com>
2019-03-03 14:40:38 -0500

    refactor

--- install.sh
+++ install.sh
@@ -11,11 +11,9 @@
 #
 
 basedir=$HOME/.dotfiles
-gitbase=git://github.com/cbeck527/dotfiles.git
-tarball=http://github.com/cbeck527/dotfiles/tarball/master
 
 function has() {
-    return $( which $1 >/dev/null )
+    return "$(type $1 >/dev/null)"
 }
 
 function note() {
@@ -27,7 +25,7 @@ function warn() {
 }
 
 function die() {
-    warn $*
+    warn "$@"
     exit 1
 }
 
@@ -35,62 +33,24 @@ function link() {
     src=$1
     dest=$2
 
-    if [ -e $dest ]; then
-        if [ -L $dest ]; then
+    if [ -e "$dest" ]; then
+        if [ -L "$dest" ]; then
             # Already symlinked -- I'll assume correctly.
             return
         else
             # Rename files with a ".old" extension.
             warn "$dest file already exists, renaming to $dest.old"
-            backup=$dest.old
-            if [ -e $backup ]; then
+            backup="$dest.old"
+            if [ -e "$backup" ]; then
                 die "$backup already exists. Aborting."
             fi
-            mv -v $dest $backup
+            mv -v "$dest" "$backup"
         fi
     fi
 
     # Update existing or create new symlinks.
-    ln -vsf $src $dest
+    ln -vsf "$src" "$dest"
 }
-
-function unpack_tarball() {
-    note "Downloading tarball..."
-    mkdir -vp $basedir
-    cd $basedir
-    tempfile=TEMP.tar.gz
-    if has curl; then
-        curl -L $tarball >$tempfile
-    elif has wget; then
-        wget -O $tempfile $tarball
-    else:
-        die "Can't download tarball."
-    fi
-    tar --strip-components 1 -zxvf $tempfile
-    rm -v $tempfile
-}
-
-if [ -e $basedir ]; then
-    # Basedir exists. Update it.
-    cd $basedir
-    if [ -e .git ]; then
-        note "Updating dotfiles from git..."
-        git pull --rebase origin master
-    else
-        unpack_tarball
-    fi
-else
-    # .dotfiles directory needs to be installed. Try downloading first with
-    # git, then use tarballs.
-    if has git; then
-        note "Cloning from git..."
-        git clone $gitbase $basedir
-        cd $basedir
-    else
-        warn "Git not installed."
-        unpack_tarball
-    fi
-fi
 
 note "Installing dotfiles..."
 for path in * ; do
@@ -99,14 +59,14 @@ for path in * ; do
             continue
             ;;
         *)
-            link $basedir/$path $HOME/.$path
+            link "$basedir/$path" "$HOME/.$path"
             ;;
     esac
 done
 
 if [ "$(uname -s)" == "Darwin" ]; then
   note "Installing macOS specific dotfiles"
-  link $basedir/osxrc $HOME/.osxrc
+  link "$basedir/osxrc" "$HOME/.osxrc"
 fi
 
 note "Installing vundle and vim plugins"
